4d0448a6e2d7a0184db5d1e0fe1446795ed42106
abdoulaye1301 <122128532+abdoulaye1301@users.noreply.github.com>
2024-12-26 23:43:51 +0000

    Update 1_🗃️_Accueil.py

--- 1_🗃️_Accueil.py
+++ 1_🗃️_Accueil.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+from PIL import Image
+
 
 st.set_page_config(page_title="ND_FA_Biostatistique", page_icon="🎢", layout="centered")
 st.sidebar.success("Selectionnez une page")
@@ -14,14 +16,16 @@ def main():
     st.markdown(
         "**Cette étude consiste à mettre en place un modèle de machine learning ou statistique qui permette de faire un pronostique sur la survenue instantanée de décès après le traitement. Pour la construction de ce modèle, nous allons utiliser les données de patients atteints d’accident cérébral vasculaire (AVC), traités et suivis.Après avoir observé les dimensions de notre base de données, nous avons constaté qu’elle contient 1053 observations réparties sur 16 variables dont 4 variables sont des variables quantitatives et 12 sont des variables qualitatives.**"
     )
+    # Ouvrez l'image
+    Image.open("biostatistique.jpg")
     # donne = st.file_uploader("Charge les données", ["xlsx"])
-    df = pd.read_excel("Donnnées_Projet_M2SID2023_2024.xlsx")
+    #df = pd.read_excel("Donnnées_Projet_M2SID2023_2024.xlsx")
     # df = pd.DataFrame(donne)
-    va = st.number_input(
-        "Le nombre d'observation à afficher", min_value=0, max_value=1050, step=1
-    )
-    if st.button("Afficher"):
-        st.write(df.head(va))
+    #va = st.number_input(
+     #   "Le nombre d'observation à afficher", min_value=0, max_value=1050, step=1
+    #)
+    #if st.button("Afficher"):
+      #  st.write(df.head(va))
 
 
 if __name__ == "__main__":
